8b48275efc517e97dd1f591939962b6b280dd752
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-29 11:22:29 +0530

    Add project files.

---
+++ Migrations/20200629051538_B2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoWebsite.Migrations
+{
+    public partial class B2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    M1 = table.Column<int>(nullable: false),
+                    M2 = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marks");
+        }
+    }
+}
